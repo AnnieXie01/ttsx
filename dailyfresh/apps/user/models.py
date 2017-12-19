df0c3208018fe31e34f240294a5b5b81934384ab
AnnieXie01 <447344240@163.com>
2017-12-19 18:25:52 +0800

    创建应用对应的表结构

--- dailyfresh/apps/user/models.py
+++ dailyfresh/apps/user/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from db.base_model import BaseModel
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+
+class User(BaseModel, AbstractUser):
+    '''用户表'''
+
+    # 用户名及密码等字段继承自AbstractUser
+    class Meta:
+        db_table = 'df_user'
+        verbose_name = '用户'
+        verbose_name_plural = verbose_name
+
+
+class Address(BaseModel):
+    user = models.ForeignKey('User', verbose_name='所属账户')
+    receiver = models.CharField(max_length=20, verbose_name='收件人')
+    addr = models.CharField(max_length=256, verbose_name='收件地址')
+    zip_code = models.CharField(max_length=6, null=True, verbose_name='邮政编码')
+    phone = models.CharField(max_length=11, verbose_name='手机号码')
+    is_default = models.BooleanField(default=False, verbose_name='是否为默认地址')
+
+    class Meta:
+        db_table = 'df_address'
+        verbose_name = '地址'
+        verbose_name_plural = verbose_name
+
